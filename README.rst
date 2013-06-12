e48b446f82a9b72bc3be9ac728b46b8477fe2453
ant30 <ant30tx@gmail.com>
2013-06-12 10:16:28 +0200

    Include empty doc files and requirements

---
+++ README.rst
@@ -0,0 +1,5 @@
+eduID Dashboard Attribute Manager Plugin
+======================================
+
+This is the Attribute Manager Plugin for the eduID Dashboard application.
+
--- setup.py
+++ setup.py
@@ -1,25 +1,47 @@
-from setuptools import setup, find_packages
-import sys, os
+import sys
+import os
 
-version = '0.1'
+from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
+
+version = '0.1dev'
+
+requires = [
+    'pymongo==2.5.1',
+    'eduid_am',
+]
+
+testing_extras = [
+    'nose==1.2.1',
+    'nosexcover==1.0.8',
+    'coverage==3.6',
+]
+
 
 setup(name='eduid-dashboard-amp',
       version=version,
-      description="",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      description="eduID DashboardAttribute Manager Plugin",
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      ],
       keywords='',
       author='NORDUnet A/S',
       author_email='',
       url='',
       license='',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
+      install_requires=requires,
+      extra_requires={
+          'testing': testing_extras,
+      },
+      test_suite='eduid_dashboard_amp',
       entry_points="""
       # -*- Entry points: -*-
       """,
