0546678f958d3876d15a1de39137be2962365b49
Johan Lundberg <lundberg@nordu.net>
2014-11-27 14:25:09 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
-version = '0.2.6'
+version = '0.2.7'
 
 requires = [
     'pymongo==2.6.3',	# CI fails to build unless a version (same as in eduid_am) is required here :(
